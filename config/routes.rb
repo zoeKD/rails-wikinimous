d53fd27457ddae1877fbcd219b31fd7f157d86a6
Zoe Cade <zoe.cade@gmail.com>
2017-11-21 16:50:02 +0100

    coded a basic wiki with CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'articles/create'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  get 'articles/update'
+
+  get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
